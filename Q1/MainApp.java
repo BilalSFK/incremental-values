598bb29250858c4a4423b16da8ee8704187ed5ef
BİLAL <bllsfk.3984@gmail.com>
2022-08-29 14:38:02 +0300

    committed by Bilal

---
+++ Q1/MainApp.java
@@ -0,0 +1,18 @@
+package Q1;
+public class MainApp {
+
+    public class Test {
+        public static void main(String[] args) {
+            int x = 1;
+            int y = 1;
+
+            if (x++ < ++y) {
+                System.out.print("hello");
+            } else {
+                System.out.print("welcome");
+            }
+            System.out.println("Log" + x + ":" + y);
+
+        }
+    }
+}
